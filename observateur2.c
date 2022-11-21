8d94faa838bc2f4a9899abf319d1197c48b4ab9e
JustAlternate <loic.weber@etu.univ-grenoble-alpes.fr>
2022-11-21 16:01:54 +0100

    ajout des test et de observateur2

---
+++ observateur2.c
@@ -0,0 +1,41 @@
+
+#include "observateur.h"
+
+#include <stdio.h>
+
+int init_etat_obs(){
+  return 0;
+}
+ 
+int faire_transition(etat e, Type_Commande c){
+  /* 
+  Arguments : Un etat e , une command c  
+  Renvoie l'état suivant en fonction de la command 
+  */
+  switch(e){
+    case 0:
+      if (c == Gauche || c == Droite){
+        return 0;
+      }
+      if (c == Mesurer){
+        return 1;
+      }
+      if (c == Avancer){
+        return 2;
+      } 
+      return 0;
+    case 1:
+      if (c == Gauche || c == Droite || c == Avancer){
+        return 0;
+      }
+      if (c == Mesurer){
+        return 1;
+      }
+      return 1; 
+    case 2:
+      return 2; 
+  }
+}
+
+
+
