103b66cc60c34b338f2f1abe818d392307e18a28
JustAlternate <loic.weber@etu.univ-grenoble-alpes.fr>
2022-11-26 13:44:58 +0100

    Changement de comment fonctionne testing.sh, maintenant on peut faire uaussi la commande ./lance_test_tous_les_programmes_sur_tout_les_curiosity.sh pour exexuter tout les programme_test_* (il faut juste reparer le make file pour pouvoir utiliser curiosirt-test

---
+++ fichiers_test_TP7/lance_test_tous_les_programmes_sur_tout_les_curiosity.sh
@@ -0,0 +1,3 @@
+echo "Lancement de tous les programme_test* sur tout les curiosity-test*"
+echo "================================================================="
+for i in programme_test_* ; do ./testing.sh "$i"; done
--- fichiers_test_TP7/testing.sh
+++ fichiers_test_TP7/testing.sh
@@ -1,20 +1,25 @@
-echo 0:
-./curiosity-test0 $1
-echo 1:
-./curiosity-test1 $1
-echo 2:
-./curiosity-test2 $1
-echo 3:
-./curiosity-test3 $1
-echo 4:
-./curiosity-test4 $1
-echo 5:
-./curiosity-test5 $1
-echo 6:
-./curiosity-test6 $1
-echo 7:
-./curiosity-test7 $1
-echo 8:
-./curiosity-test8 $1
-echo 9:
-./curiosity-test9 $1
+if [ $# -eq 1 ]
+then
+  echo curiosity-test0 avec $1 :
+  ./../curiosity-test0 $1
+  echo curiosity-test1 avec $1 :
+  ./../curiosity-test1 $1
+  echo curiosity-test2 avec $1 :
+  ./../curiosity-test2 $1
+  echo curiosity-test3 avec $1 :
+  ./../curiosity-test3 $1
+  echo curiosity-test4 avec $1 :
+  ./../curiosity-test4 $1
+  echo curiosity-test5 avec $1 :
+  ./../curiosity-test5 $1
+  echo curiosity-test6 avec $1 :
+  ./../curiosity-test6 $1
+  echo curiosity-test7 avec $1 :
+  ./../curiosity-test7 $1
+  echo curiosity-test8 avec $1 :
+  ./../curiosity-test8 $1
+  echo curiosity-test9 avec $1 :
+  ./../curiosity-test9 $1
+else
+  echo "usage : ./testing <nom_du_programme_test>"
+fi
