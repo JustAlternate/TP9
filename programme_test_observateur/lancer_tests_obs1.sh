fd9838db03281760c9a87198d53f9369339f422f
Pascal Isak <pascali@etu.univ-grenoble-alpes.fr>
2022-11-27 19:39:50 +0100

    refonte des lancer_tests_obs.sh

--- programme_test_observateur/lancer_tests_obs1.sh
+++ programme_test_observateur/lancer_tests_obs1.sh
@@ -2,16 +2,33 @@
 echo ============================
 echo Lancement des tests sur obs1 
 echo ============================
-echo test correct et accepte:
-.././curiosity-obs programme_test_observateur/terrain_test programme_test_observateur/programme_test_obs1_correct_accepte
-echo DONE
-echo test correct et rejete:
-.././curiosity-obs programme_test_observateur/terrain_test programme_test_observateur/programme_test_obs1_correct_rejete
-echo DONE
-echo test incorrect et accepte:
-.././curiosity-obs programme_test_observateur/terrain_test programme_test_observateur/programme_test_obs1_incorrect_accepte
-echo DONE
-echo test incorrect et rejete:
-.././curiosity-obs programme_test_observateur/terrain_test programme_test_observateur/programme_test_obs1_incorrect_rejete
-echo DONE 
-echo =============================
+if [ $0 = "./lancer_tests_obs1.sh" ]
+then
+    echo test correct et accepte:
+    .././curiosity-obs terrain_test programme_test_obs1_correct_accepte
+    echo  
+    echo test correct et rejete:
+    .././curiosity-obs terrain_test programme_test_obs1_correct_rejete
+    echo  
+    echo test incorrect et accepte:
+    .././curiosity-obs terrain_test programme_test_obs1_incorrect_accepte
+    echo  
+    echo test incorrect et rejete:
+    .././curiosity-obs terrain_test programme_test_obs1_incorrect_rejete
+    echo   
+    echo =============================
+else
+    echo test correct et accepte:
+    ./curiosity-obs programme_test_observateur/terrain_test programme_test_observateur/programme_test_obs1_correct_accepte
+    echo  
+    echo test correct et rejete:
+    ./curiosity-obs programme_test_observateur/terrain_test programme_test_observateur/programme_test_obs1_correct_rejete
+    echo  
+    echo test incorrect et accepte:
+    ./curiosity-obs programme_test_observateur/terrain_test programme_test_observateur/programme_test_obs1_incorrect_accepte
+    echo  
+    echo test incorrect et rejete:
+    ./curiosity-obs programme_test_observateur/terrain_test programme_test_observateur/programme_test_obs1_incorrect_rejete
+    echo   
+    echo =============================
+fi
--- programme_test_observateur/lancer_tests_obs2.sh
+++ programme_test_observateur/lancer_tests_obs2.sh
@@ -1,17 +1,34 @@
-# Ce fichier permet de lancer automatiquement tout les tests de l'observateur 1
+# Ce fichier permet de lancer automatiquement tout les tests de l'observateur 2
 echo ============================
 echo Lancement des tests sur obs2 
 echo ============================
-echo test correct et accepte:
-.././curiosity-obs2 programme_test_observateur/terrain_test programme_test_observateur/programme_test_obs2_correct_accepte
-echo DONE
-echo test correct et rejete:
-.././curiosity-obs programme_test_observateur/terrain_test programme_test_observateur/programme_test_obs2_correct_rejete
-echo DONE
-echo test incorrect et accepte:
-.././curiosity-obs programme_test_observateur/terrain_test programme_test_observateur/programme_test_obs2_incorrect_accepte
-echo DONE
-echo test incorrect et rejete:
-.././curiosity-obs programme_test_observateur/terrain_test programme_test_observateur/programme_test_obs2_incorrect_rejete
-echo DONE 
-echo =============================
+if [ $0 = "./lancer_tests_obs2.sh" ]
+then
+    echo test correct et accepte:
+    .././curiosity-obs2 terrain_test programme_test_obs2_correct_accepte
+    echo 
+    echo test correct et rejete:
+    .././curiosity-obs2 terrain_test programme_test_obs2_correct_rejete
+    echo 
+    echo test incorrect et accepte:
+    .././curiosity-obs2 terrain_test programme_test_obs2_incorrect_accepte
+    echo 
+    echo test incorrect et rejete:
+    .././curiosity-obs2 terrain_test programme_test_obs2_incorrect_rejete
+    echo 
+    echo =============================
+else
+    echo test correct et accepte:
+    ./curiosity-obs2 programme_test_observateur/terrain_test programme_test_observateur/programme_test_obs2_correct_accepte
+    echo 
+    echo test correct et rejete:
+    ./curiosity-obs2 programme_test_observateur/terrain_test programme_test_observateur/programme_test_obs2_correct_rejete
+    echo 
+    echo test incorrect et accepte:
+    ./curiosity-obs2 programme_test_observateur/terrain_test programme_test_observateur/programme_test_obs2_incorrect_accepte
+    echo 
+    echo test incorrect et rejete:
+    ./curiosity-obs2 programme_test_observateur/terrain_test programme_test_observateur/programme_test_obs2_incorrect_rejete
+    echo 
+    echo =============================
+fi
