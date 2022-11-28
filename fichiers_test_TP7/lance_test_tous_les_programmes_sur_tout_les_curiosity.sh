90fa210266ea8f25b47ed35b2b64b0a316b0cf43
JustAlternate <loic.weber@etu.univ-grenoble-alpes.fr>
2022-11-28 10:03:28 +0100

    lets go

--- fichiers_test_TP7/lance_test_tous_les_programmes_sur_tout_les_curiosity.sh
+++ fichiers_test_TP7/lance_test_tous_les_programmes_sur_tout_les_curiosity.sh
@@ -1,3 +1,22 @@
+# A N'EXECUTER QUE AVEC LE MAKEFILE ou bien depuis LE DOSSIER TP9. (make tests_TP7)
 echo "Lancement de tous les programme_test* sur tout les curiosity-test*"
 echo "================================================================="
-for i in programme_test_* ; do ./testing.sh "$i"; done
+if [ $0 == './lance_test_tous_les_programmes_sur_tout_les_curiosity.sh' ]
+then
+  for i in programme_test_* ; do
+    echo $i
+    ./testing.sh $i
+  done
+else
+  if [ $0 == 'fichiers_test_TP7/./lance_test_tous_les_programmes_sur_tout_les_curiosity.sh' ]
+  then
+    for i in fichiers_test_TP7/programme_test_* ; do
+      echo $i
+      fichiers_test_TP7/./testing.sh $i
+    done
+  else
+    echo "Usage: lancé soit depuis le dossier fichiers_test_TP7 ou bien avec le makefile. "
+  fi
+
+fi
+
--- fichiers_test_TP7/testing.sh
+++ fichiers_test_TP7/testing.sh
@@ -1,25 +1,51 @@
+# A NEXECUTER QUE AVEC LE MAKE FILE EN FAISANT make tests_curiosity
 if [ $# -eq 1 ]
 then
-  echo curiosity-test0 avec $1 :
-  ./../curiosity-test0 $1
-  echo curiosity-test1 avec $1 :
-  ./../curiosity-test1 $1
-  echo curiosity-test2 avec $1 :
-  ./../curiosity-test2 $1
-  echo curiosity-test3 avec $1 :
-  ./../curiosity-test3 $1
-  echo curiosity-test4 avec $1 :
-  ./../curiosity-test4 $1
-  echo curiosity-test5 avec $1 :
-  ./../curiosity-test5 $1
-  echo curiosity-test6 avec $1 :
-  ./../curiosity-test6 $1
-  echo curiosity-test7 avec $1 :
-  ./../curiosity-test7 $1
-  echo curiosity-test8 avec $1 :
-  ./../curiosity-test8 $1
-  echo curiosity-test9 avec $1 :
-  ./../curiosity-test9 $1
+  if [ $0 == 'fichiers_test_TP7/./testing.sh' ]
+  then
+    echo curiosity-test0 avec $1 :
+    ./curiosity-test0 fichiers_test_TP7/$1
+    echo curiosity-test1 avec $1 :
+    ./curiosity-test1 fichiers_test_TP7/$1
+    echo curiosity-test2 avec $1 :
+    ./curiosity-test2 fichiers_test_TP7/$1
+    echo curiosity-test3 avec $1 :
+    ./curiosity-test3 fichiers_test_TP7/$1
+    echo curiosity-test4 avec $1 :
+    ./curiosity-test4 fichiers_test_TP7/$1
+    echo curiosity-test5 avec $1 :
+    ./curiosity-test5 fichiers_test_TP7/$1
+    echo curiosity-test6 avec $1 :
+    ./curiosity-test6 fichiers_test_TP7/$1
+    echo curiosity-test7 avec $1 :
+    ./curiosity-test7 fichiers_test_TP7/$1
+    echo curiosity-test8 avec $1 :
+    ./curiosity-test8 fichiers_test_TP7/$1
+    echo curiosity-test9 avec $1 :
+    ./curiosity-test9 fichiers_test_TP7/$1
+  else
+    echo curiosity-test0 avec $1 :
+    .././curiosity-test0 fichiers_test_TP7/$1
+    echo curiosity-test1 avec $1 :
+    .././curiosity-test1 fichiers_test_TP7/$1
+    echo curiosity-test2 avec $1 :
+    .././curiosity-test2 fichiers_test_TP7/$1
+    echo curiosity-test3 avec $1 :
+    .././curiosity-test3 fichiers_test_TP7/$1
+    echo curiosity-test4 avec $1 :
+    .././curiosity-test4 fichiers_test_TP7/$1
+    echo curiosity-test5 avec $1 :
+    .././curiosity-test5 fichiers_test_TP7/$1
+    echo curiosity-test6 avec $1 :
+    .././curiosity-test6 fichiers_test_TP7/$1
+    echo curiosity-test7 avec $1 :
+    .././curiosity-test7 fichiers_test_TP7/$1
+    echo curiosity-test8 avec $1 :
+    .././curiosity-test8 fichiers_test_TP7/$1
+    echo curiosity-test9 avec $1 :
+    .././curiosity-test9 fichiers_test_TP7/$1
+  fi
+
 else
   echo "usage : ./testing <nom_du_programme_test>"
 fi
